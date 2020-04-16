881e968dd675c29d206dae1a48ae4d9fdfef4b79
xiaoClassmate <s1410643030@gms.nutc.edu.tw>
2020-04-17 00:26:47 +0800

    daylight_and_dark

--- MidtermExamProject/mid_1410643030/app/src/main/java/com/example/xiaoclassmate/MainActivity.java
+++ MidtermExamProject/mid_1410643030/app/src/main/java/com/example/xiaoclassmate/MainActivity.java
@@ -3,28 +3,50 @@ package com.example.xiaoclassmate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageButton counter, scroll, poker, bmi, temperature, pizza, steak, tetris, settings;
+    private ImageButton counter, scroll, poker, bmi, temperature, pizza, steak, tetris;
+    private TextView title;
+    private Button switch_button;
+    private int theme = R.style.AppTheme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 判斷是否有主題儲存
+        if (savedInstanceState != null) {
+            theme = savedInstanceState.getInt("theme");
+            setTheme(theme);
+        }
         setContentView(R.layout.activity_main);
+        switch_button = (Button) findViewById(R.id.theme);
+        switch_button.setOnClickListener(new View.OnClickListener() {
 
-        counter = (ImageButton)findViewById(R.id.counter);
-        scroll = (ImageButton)findViewById(R.id.scroll);
-        poker = (ImageButton)findViewById(R.id.poker);
-        bmi = (ImageButton)findViewById(R.id.bmi);
-        temperature = (ImageButton)findViewById(R.id.temperature);
-        pizza = (ImageButton)findViewById(R.id.pizza);
-        steak = (ImageButton)findViewById(R.id.steak);
-        tetris = (ImageButton)findViewById(R.id.tetris);
-        settings = (ImageButton)findViewById(R.id.settings);
+            @Override
+            public void onClick(View v) {
+                theme = (theme == R.style.AppTheme) ? R.style.NightTheme : R.style.AppTheme;
+                MainActivity.this.recreate();
+            }
+        });
+
+        counter = (ImageButton) findViewById(R.id.counter);
+        scroll = (ImageButton) findViewById(R.id.scroll);
+        poker = (ImageButton) findViewById(R.id.poker);
+        bmi = (ImageButton) findViewById(R.id.bmi);
+        temperature = (ImageButton) findViewById(R.id.temperature);
+        pizza = (ImageButton) findViewById(R.id.pizza);
+        steak = (ImageButton) findViewById(R.id.steak);
+        tetris = (ImageButton) findViewById(R.id.tetris);
+        title = (TextView) findViewById(R.id.myself_text);
+
+        title.setTextColor(Color.rgb(0, 0, 0));
 
         counter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -98,13 +120,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        settings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, SettingsActivity.class);
-                startActivity(intent);
-            }
-        });
+//        settings.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent();
+//                intent.setClass(MainActivity.this, SettingsActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("theme", theme);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        theme = savedInstanceState.getInt("theme");
     }
 }
